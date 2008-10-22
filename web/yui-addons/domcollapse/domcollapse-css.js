1ed394fa9ff0e5d3000b0e9e3c2acf63465d453f
Benjamin.Meynell <Benjamin.Meynell@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2008-10-22 13:27:00 -0500

    bmeynell: created 1.1 and 1.2 branches; moved trunk copy to 1.1; copied 1.1 copy to 1.2 branch [ysfYUIPlugin]
    git-svn-id: http://svn.symfony-project.com/plugins/ysfYUIPlugin/trunk@12324 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- web/yui-addons/domcollapse/domcollapse-css.js
+++
@@ -1,9 +0,0 @@
-﻿if(YAHOO && YAHOO.otorg && YAHOO.otorg.DomCollapse){
-	YAHOO.otorg.DomCollapse.css = {
-		triggerClass:'trigger',
-		hideClass:'hide',
-		parentClass:'parent',
-		openClass:'open'
-	}
-}
-	
